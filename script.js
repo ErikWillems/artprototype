a77ba0abac70cd3b871734c726bf8cccbab40836
Erik Willems <webmaster@rocketspeed.nl>
2026-02-14 11:13:10 +0100

    update

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 (function () {
-  let gameQuestion = 0;
+  let gameAction = "hands";
   let gameHands = 0;
   let gameFingers = 0;
 
-  d3.selectAll(".question button").on("click", function () {
+  d3.selectAll(".container[data-action] button").on("click", function () {
     const button = d3.select(this);
     const action = button.attr("data-action");
     const hands = button.attr("data-hands");
     const fingers = button.attr("data-fingers");
-    if (action === "next") {
-      gameQuestion += 1;
+    if (typeof action === "string") {
+      gameAction = action;
     }
     if (typeof hands === "string") {
       gameHands = parseInt(hands, 10);
@@ -18,39 +18,32 @@
     if (typeof fingers === "string") {
       gameFingers = parseInt(fingers, 10);
     }
-    console.log(
-      gameFingers,
-      gameHands,
-      gameFingers / gameHands ? gameHands : 1,
-    );
 
     digest();
   });
 
   const digest = function () {
-    d3.selectAll(".question").classed("d-none", function (d, i) {
-      return i !== gameQuestion;
+    d3.selectAll(".container[data-action]").classed("d-none", function () {
+      console.log(d3.select(this).attr("data-action"), gameAction);
+      return d3.select(this).attr("data-action") !== gameAction;
     });
-    d3.select(".hand-left")
-      .classed("invisible", function () {
-        return gameHands < 1;
-      })
-      .attr(
-        "src",
-        "hand-left-" +
-          Math.max(0, (gameFingers / gameHands).toString()) +
-          ".png",
-      );
-    d3.select(".hand-right")
-      .classed("invisible", function () {
-        return gameHands < 2;
-      })
-      .attr(
-        "src",
-        "hand-right-" +
-          Math.max(0, (gameFingers / gameHands).toString()) +
-          ".png",
-      );
+
+    digestHands();
+  };
+
+  const digestHands = function () {
+    const handLeft = d3.select(".hand-left");
+    const handRight = d3.select(".hand-right");
+    if (gameHands === 0) {
+      handLeft.attr("src", "hand-none.png");
+      handRight.attr("src", "hand-none.png");
+    } else if (gameHands === 1) {
+      handLeft.attr("src", "hand-" + gameFingers + ".png");
+      handRight.attr("src", "hand-none.png");
+    } else {
+      handLeft.attr("src", "hand-" + gameFingers * 0.5 + ".png");
+      handRight.attr("src", "hand-" + gameFingers * 0.5 + ".png");
+    }
   };
   digest();
 })(window);
