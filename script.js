57ded33639a11c603ee685a01d1fbd3e53dfa03e
Erik Willems <webmaster@rocketspeed.nl>
2026-02-14 15:38:09 +0100

    update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   let gameHands = 0;
   let gameFingers = 0;
 
-  d3.selectAll("div[data-action] h1").each(function () {
+  d3.selectAll("div[data-action] h1, div[data-action] p").each(function () {
     const elem = d3.select(this);
     const text = elem.text();
     elem.text("");
@@ -12,15 +12,22 @@
   });
 
   setInterval(function () {
-    d3.selectAll("div[data-action].active h1").each(function () {
-      const first = d3.select(this).select("span:first-child");
-      const last = d3.select(this).select("span:last-child");
-      const cursor = last.text().length;
-      if (last.text().length < first.text().length) {
-        const newText = last.text() + first.text()[last.text().length];
-        last.text(newText);
-      }
-    });
+    let hasJob = false;
+    d3.selectAll("div[data-action].active h1, div[data-action].active p").each(
+      function () {
+        if (hasJob) {
+          return;
+        }
+        const first = d3.select(this).select("span:first-child");
+        const last = d3.select(this).select("span:last-child");
+        const cursor = last.text().length;
+        if (last.text().length < first.text().length) {
+          hasJob = true;
+          const newText = last.text() + first.text()[last.text().length];
+          last.text(newText);
+        }
+      },
+    );
     //
   }, 50);
 
